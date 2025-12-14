58df353745e48bb69f196c1b37b906eb560d33f6
arcaload <56554857+gatinaisapp@users.noreply.github.com>
2025-12-15 00:59:53 +0100

    Add RexGradDataset and update data module setup

--- data/loader.py
+++ data/loader.py
@@ -3,19 +3,50 @@ import torchvision.datasets as datasets
 from torch.utils.data import DataLoader
 from torch.utils.data import random_split
 import pytorch_lightning as pl
+from torchvision import transforms
+from transformers import AutoTokenizer
 
 
+class RexGradDataset(IterableDataset):
+    def __init__(self,split: str,tokenizer, image_transform):
+        self.ds=load_dataset("rajpurkarlab/RexGradient-160K",split=split,streaming=True)
+        self.tokenizer=tokenizer
+        self.image_transform=image_transform
+    def __iter__(self):
+        for sample in self.ds:
+            image=self.image_transform(sample["image"])
+            text=sample['report']
+            encoding=self.tokenizer(text,truncation=True,padding="max_length",max_lentgh=256)
+            yield {
+                "input_ids:torch.tensor(encoding["input_ids"]),
+                "pixel": image,
+                "attention_mask": torch.tensor(enc["attention_mask"])
+            }
+            
 class RexGradDataModule(pl.LightningDataModule):
-    def __init__(self, data_dir, batch_size, num_workers):
+    def __init__(self, hf_dir: str, batch_size: int=32, num_workers: int=4):
         super().__init__()
-        self.data_dir = data_dir
+        self.hf_dir = hf_dir
         self.batch_size = batch_size
         self.num_workers = num_workers
+        self.tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 
-    def prepare_data(self):
-
-    def setup(self, stage):
-
+   def setup(self, stage=None):
+        self.train_ds = RexGradientDataset(
+            split="train",
+            tokenizer=self.tokenizer,
+            image_transform=get_image_transform("train"),
+        )
+       self.val_ds = RexGradientDataset(
+            split="validation",
+            tokenizer=self.tokenizer,
+            image_transform=get_image_transform("eval"),
+        )
+        self.test_ds = RexGradientDataset(
+            split="test",
+            tokenizer=self.tokenizer,
+            image_transform=get_image_transform("eval"),
+        )
 
     def train_dataloader(self):
         return DataLoader(
