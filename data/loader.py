c4abc35f88cd20196b5bb90aef572db603a5a02d
arcaload <56554857+gatinaisapp@users.noreply.github.com>
2025-12-15 00:28:35 +0100

    Add RexGradDataModule for data handling
    Implemented RexGradDataModule for data loading in PyTorch.

--- data/loader.py
+++ data/loader.py
@@ -1,1 +1,42 @@
+import torch
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+import pytorch_lightning as pl
 
+
+class RexGradDataModule(pl.LightningDataModule):
+    def __init__(self, data_dir, batch_size, num_workers):
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+    def prepare_data(self):
+
+    def setup(self, stage):
+
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_ds,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=True,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.val_ds,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=False,
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            self.test_ds,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=False,
+        )
