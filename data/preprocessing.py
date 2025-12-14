b3eb2d99bcdad0f08c693b852c3d22ca61ba6293
arcaload <56554857+gatinaisapp@users.noreply.github.com>
2025-12-15 00:28:48 +0100

    Add image transformation

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -1,1 +1,13 @@
+from torchvision import transforms
 
+def get_image_transform(stage: str = "train"):
+    if stage == "train":
+        return transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+        ])
+    return transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+    ])
